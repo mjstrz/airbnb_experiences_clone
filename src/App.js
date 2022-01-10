f4000f1baa970ecb822215d138ead994eaf7438e
mjstrz <strzelecki.monica7@gmail.com>
2022-01-10 13:25:56 -0500

    Card Component created; Life Lessons with Katie Zefares - 1.10.2022 13:25

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
-import Hero from "./components/hero"
+import Hero from "./components/hero";
+import Card from "./components/card";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
