e46beb211418fddd36558ad0424572c98d5ef964
mjstrz <strzelecki.monica7@gmail.com>
2022-01-09 22:59:35 -0500

    Project setup: Hero - 1.9.2022 23:00

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from "./components/hero"
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
