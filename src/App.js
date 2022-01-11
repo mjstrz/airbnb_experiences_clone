92a9bc471fdd7d198f5d76b72af84fc76a288f76
mjstrz <strzelecki.monica7@gmail.com>
2022-01-11 17:52:00 -0500

    Map experiences data into components - 1.11.2022 17:51

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from "./components/hero";
 import Card from "./components/card";
+import data from "./components/data";
+
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+
 
 export default function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
-    </div>
-  )
+  // <Hero />
+const cards = data.map(item => {
+return (
+  <Card 
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+  />
+)
+})        
+
+return (
+<div>
+  <Navbar />
+  {cards}
+</div>
+)
 }
