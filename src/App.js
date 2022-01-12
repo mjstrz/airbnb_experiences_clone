13a75faf7caaa6b85526d1d224cb079928611d17
mjstrz <strzelecki.monica7@gmail.com>
2022-01-11 21:20:10 -0500

    Added the key prop on App.js; flexbox applied to .card section

--- src/App.js
+++ src/App.js
@@ -5,25 +5,13 @@ import Hero from "./components/hero";
 import Card from "./components/card";
 import data from "./components/data";
 
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
-
 
 export default function App() {
   // <Hero />
 const cards = data.map(item => {
 return (
   <Card 
+      key={item.id}
       img={item.coverImg}
       rating={item.stats.rating}
       reviewCount={item.stats.reviewCount}
@@ -37,7 +25,9 @@ return (
 return (
 <div>
   <Navbar />
-  {cards}
+  <section className='cards-list'>
+    {cards}
+  </section>
 </div>
 )
 }
--- src/components/card.js
+++ src/components/card.js
@@ -11,8 +11,8 @@ export default function Card(props) {
                 <span className="gray">({props.reviewCount}) •</span>
                 <span className="gray">{props.country}</span>
             </div>
-            <p>{props.title}</p>
-            <p><strong>From ${props.price}</strong> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><strong>From ${props.price}</strong> / person</p>
         </div>
     )
 }
